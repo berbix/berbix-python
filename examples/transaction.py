baac01b3a2f7b176eda546a3f0bd0c724916c7f0
Yifeng Ding <yifeng@berbix.com>
2020-09-18 17:17:35 -0700

    run prettier on code

--- examples/transaction.py
+++ examples/transaction.py
@@ -1,14 +1,15 @@
+import berbix
 import sys
 import os
 import pprint
 import time
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-import berbix
 
 cl = berbix.Client(api_secret=os.environ['BERBIX_DEMO_API_SECRET'])
 
-tokens = cl.create_transaction(customer_uid="this_is_a_customer_uid", template_key=os.environ["BERBIX_DEMO_TEMPLATE_KEY"])
+tokens = cl.create_transaction(
+    customer_uid="this_is_a_customer_uid", template_key=os.environ["BERBIX_DEMO_TEMPLATE_KEY"])
 
 pprint.pprint(tokens)
 
@@ -21,14 +22,16 @@ transaction = cl.fetch_transaction(tokens)
 
 pprint.pprint(transaction)
 
-completed_transaction = cl.override_transaction(tokens, response_payload="us-dl", flags=["id_under_18", "id_under_21"])
+completed_transaction = cl.override_transaction(
+    tokens, response_payload="us-dl", flags=["id_under_18", "id_under_21"])
 
 pprint.pprint(completed_transaction)
 
-updated_transaction = cl.update_transaction(tokens, action="accept", note="making an exception for this person")
+updated_transaction = cl.update_transaction(
+    tokens, action="accept", note="making an exception for this person")
 
 pprint.pprint(updated_transaction)
 
 # Uncomment the lines below to delete the newly-created transaction
 #deleted_transaction = cl.delete_transaction(tokens)
-#pprint.pprint(deleted_transaction)
+# pprint.pprint(deleted_transaction)
